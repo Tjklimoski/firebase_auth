e7300300b8a3566a45ff83a7fdadbe8a6c1df0c9
TJ Klimoski <tjklimoski@gmail.com>
2023-06-27 17:55:20 -0400

    Center Sign Up form in middle of page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,13 @@
+import { Container } from "react-bootstrap";
 import SignUp from "./SignUp";
 
 function App() {
 
-  return <SignUp />
+  return (
+    <Container className="d-flex flex-column align-items-center justify-content-center vh-100">
+      <SignUp />
+    </Container>
+  )
 }
 
 export default App
