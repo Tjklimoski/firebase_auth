adb07b32e31286160adb3df54a20fc9e1df68f23
TJ Klimoski <tjklimoski@gmail.com>
2023-06-28 13:32:59 -0400

    Create Dashboard page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useState } from "react"
+import { Alert, Button, Card } from "react-bootstrap"
+import { useAuth } from "../context/AuthContext"
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
+  const [error, setError] = useState('')
+  const { user, logout } = useAuth();
+
+  function handleLogout() {
+
+  }
+
   return (
-    <div>Dashboard</div>
+    <>
+      <Card style={{ width: '100%', maxWidth: '400px' }}>
+        <Card.Body>
+          <h2 className="text-center mb-4">Dashboard</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email: </strong>{user?.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        <Button variant="link" onClick={handleLogout}>Log Out</Button>
+      </div>
+    </>
   )
 }
